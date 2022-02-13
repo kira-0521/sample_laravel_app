b230f3e8a51740b2c05eae8887968a174de0fd16
kira-0521 <kiratanaka0521@gmail.com>
2022-02-13 21:48:34 +0900

    feature: todoを保存する処理を追加保存する処理を追加（todoモデルをコントローラーで呼び出し）

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    // コンストラクタインジェクション
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function create()
     {
         return view('todo.create');
@@ -16,7 +25,11 @@ class TodoController extends Controller
     // Controllerに定義するメソッドは自動的に使える
     public function store(Request $request)
     {
-        dd($request->all());
+        $inputs = $request->all();
+        // dd($this->todo);
+        $this->todo->fill($inputs);
+        dd($this->todo);
+        $this->todo->save();
     }
 
 }
